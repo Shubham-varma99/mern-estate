b2490dbeb240c01fd4a944951c644492f3cf7431
Shubham Varma <shubhamverma9602@gmail.com>
2023-11-08 19:37:12 +0530

    Create user model

---
+++ api/models/user_model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required:true, // to protect our database from different type inputs
+        unique:true  // No one has similar username which is owned by person.
+    },
+    email: {
+        type:String,
+        required:true, // to protect our database from different type inputs
+        unique:true  // No one has similar username which is owned by person.
+    },
+    password: {
+        type:String,
+        required:true, // to protect our database from different type inputs
+    },
+},{timestamps:true}); // timestamps: true which is going to tell the mongodb two important info, one is time of creation and other is time of update of users. 
+
+const User = mongoose.model("User",userSchema);
+
+export default User;

d7514f616777289dcf1f8c45da558932fd51fd65
Shubham Varma <shubhamverma9602@gmail.com>
2023-11-08 13:59:58 +0530

    Create Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,14 @@ import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import About from './pages/About'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
-    <BrowserRouter>   
+    // 14 line we put header because we need to manage all those route in a header component.
+    <BrowserRouter>  
+    <Header/> 
       <Routes>
          <Route path='/' element={<Home></Home>}></Route>
          <Route path='/sign-in' element={<SignIn></SignIn>}></Route>

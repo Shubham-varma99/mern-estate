82cd999545c2a14d660c40237249730cc268b093
Shubham Varma <shubhamverma9602@gmail.com>
2023-11-19 12:22:30 +0530

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute.jsx'
 import CreateListing from './pages/CreateListing.jsx'
 import UpdateListing from './pages/UpdateListing.jsx'
 import Listing from './pages/Listing.jsx';
+import Search from './pages/Search.jsx'
 
 
 export default function App() {
@@ -23,6 +24,7 @@ export default function App() {
          <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
          <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
          <Route path='/listing/:listingId' element={<Listing></Listing>}></Route>
+         <Route path='/search' element={<Search></Search>}></Route>
          <Route element={<PrivateRoute></PrivateRoute>}>
          <Route path='/profile' element={<Profile></Profile>}></Route>
          <Route path='/create-listing' element={<CreateListing></CreateListing>}></Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,27 @@
 import { FaSearch } from "react-icons/fa";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState("");
-  const handleSubmit =(evt)=>{
+  const handleSubmit = (evt) => {
     evt.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('searchTerm',searchTerm);
+    urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
-    navigate(`/search/${searchQuery}`);
-  }
+    navigate(`/search?${searchQuery}`);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if(searchTermFromUrl){
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
-  },[]);
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -32,19 +32,22 @@ export default function Header() {
           </h1>
         </Link>
 
-        <form onClick={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onClick={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none"
             value={searchTerm}
-            onChange={(evt)=>setSearchTerm(evt.target.value)}
+            onChange={(evt) => setSearchTerm(evt.target.value)}
           />
           <button>
-          <FaSearch className="text-slate-600 " />
+            <FaSearch className="text-slate-600 " />
           </button>
-          </form>
-         <ul className="flex gap-4">
+        </form>
+        <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline ">
               Home

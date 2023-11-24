0a47cd09fd2c40c5ba1f6f9641c098e6fcf2cbbd
Shubham Varma <shubhamverma9602@gmail.com>
2023-11-24 22:06:35 +0530

    Changes in createlisting functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -274,7 +274,6 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">(Rs. / month)</span>
                 {formData.type === 'rent' && (
                   <span className='text-xs'>(Rs. / month)</span>
                 )}
@@ -293,7 +292,6 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Discounted price</p>
-                <span className="text-xs">(Rs. / month)</span>
                 {formData.type === 'rent' && (
                     <span className='text-xs'>(Rs. /month)</span>
                 )}
